a654d15f75af5a8abcd597235c26b2d097b569a0
Karen <to.karenngo@gmail.com>
2021-12-01 21:51:45 -0500

    key for short messages added

--- input.js
+++ input.js
@@ -35,6 +35,12 @@ const handleUserInput = function (key) {
   if (key === 'd') {
     connection.write('Move: right');
   }
+  if (key === 'e') {
+    connection.write('Say: I\'ll eat you');
+  }
+  if (key === 'q') {
+    connection.write('Say: CYA L8R');
+  }
 };
 
 
